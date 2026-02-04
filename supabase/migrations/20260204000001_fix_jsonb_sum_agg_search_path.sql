d8da2a3925c1d1fb4a2a76a1515deaab57ad5eb0
Zack Sikkink <ted2ce@virginia.edu>
2026-02-03 21:58:27 -0500

    Fix jsonb_sum_agg search_path for aggregate

--- supabase/migrations/20260204000001_fix_jsonb_sum_agg_search_path.sql
+++ supabase/migrations/20260204000001_fix_jsonb_sum_agg_search_path.sql
@@ -1,14 +1,6 @@
 begin;
 
-update pg_proc
-set proconfig = coalesce(
-  array_append(
-    array_remove(proconfig, 'search_path=pg_catalog,public'),
-    'search_path=pg_catalog,public'
-  ),
-  array['search_path=pg_catalog,public']
-)
-where proname = 'jsonb_sum_agg'
-  and pronamespace = (select oid from pg_namespace where nspname = 'public');
+alter aggregate public.jsonb_sum_agg(jsonb)
+  set (search_path = 'pg_catalog, public');
 
 commit;
